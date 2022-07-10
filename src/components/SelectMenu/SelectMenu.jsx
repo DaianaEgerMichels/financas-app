d394a84617bcdb533d5a531e668e2517d1f3f50f
Daiana Michels <daianaem.12@gmail.com>
2022-07-10 18:46:01 -0300

    feat: cria componente de select para seleção do mês

---
+++ src/components/SelectMenu/SelectMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SelectMenu(props) {
+
+    const options = props.lista.map(option => {
+        <option value={option.value}>{option.label}</option>
+    })
+
+  return (
+    <select {...props}>{options}</select>
+  )
+}
+
+export default SelectMenu;
