2404c89bb1d902cb8f6491cd029c516d62b7947a
Daiana Michels <daianaem.12@gmail.com>
2022-07-10 22:10:38 -0300

    feat: implementa chamada para as funções nas ações

--- src/components/LancamentosTable/LancamentosTable.jsx
+++ src/components/LancamentosTable/LancamentosTable.jsx
@@ -1,35 +1,49 @@
-import React from 'react'
+import React from "react";
+import currencyFormatter from "currency-formatter";
+import { MdEdit, MdDelete } from "react-icons/md";
 
 function LancamentosTable(props) {
-
-    const rows = props.lancamentos.map(lancamento => {
-        return (
-            <tr key={lancamento.id}>
-                <td>{lancamento.descricao}</td>
-                <td>{lancamento.valor}</td>
-                <td>{lancamento.tipo}</td>
-                <td>{lancamento.mes}</td>
-                <td>{lancamento.status}</td>
-                <td>{}</td>
-            </tr>
-        )
-    })
+  const rows = props.lancamentos.map((lancamento) => {
+    return (
+      <tr key={lancamento.id}>
+        <td>{lancamento.descricao}</td>
+        <td>
+          {currencyFormatter.format(lancamento.valor, { locale: "pt-BR" })}
+        </td>
+        <td>{lancamento.tipo}</td>
+        <td>{lancamento.mes}</td>
+        <td>{lancamento.status}</td>
+        <td>
+          <button onClick={e => props.editAction(lancamento.id)} className="btn btn-primary">
+            <abbr title="Editar">
+              <MdEdit />
+            </abbr>
+          </button>
+          <button onClick={e => props.deleteAction(lancamento.id)} className="btn btn-danger">
+            <abbr title="Deletar">
+              <MdDelete />
+            </abbr>
+          </button>
+        </td>
+      </tr>
+    );
+  });
 
   return (
-    <table className='table table-hover'>
-        <thead>
-            <tr>
-                <th scope='col'>Descrição</th>
-                <th scope='col'>Valor</th>
-                <th scope='col'>Tipo</th>
-                <th scope='col'>Mês</th>
-                <th scope='col'>Situação</th>
-                <th scope='col'>Ações</th>
-            </tr>
-        </thead>
-        <tbody></tbody>
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Descrição</th>
+          <th scope="col">Valor</th>
+          <th scope="col">Tipo</th>
+          <th scope="col">Mês</th>
+          <th scope="col">Situação</th>
+          <th scope="col">Ações</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
     </table>
-  )
+  );
 }
 
-export default LancamentosTable
+export default LancamentosTable;
