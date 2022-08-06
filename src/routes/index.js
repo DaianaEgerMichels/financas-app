b2609f1526e6eff25bd19b4c4c88dd63238513ce
Daiana Michels <daianaem.12@gmail.com>
2022-08-05 21:00:38 -0300

    feat: adiciona proteção as rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,41 @@
 import React from "react";
-import { Routes as Switch, Route } from "react-router-dom";
+import { Routes as Switch, Route, Redirect } from "react-router-dom";
 import Login from "../pages/Login/Login"
 import SignUp from "../pages/SignUp/SignUp";
 import Home from "../pages/Home/Home";
 import ConsultReleases from "../pages/ConsultReleases/ConsultReleases";
 import RegisterReleases from "../pages/RegisterReleases/RegisterReleases";
 
+const isUsuarioAutenticado = ()=>{
+    return false;
+}
+
+function RotaAutenticada({component: Component, ...props}){
+  return (
+    <Route {...props} render={(componentProps)=>{
+        if(isUsuarioAutenticado()){
+          return(
+            <Component {...componentProps}/>
+          )
+        }else{
+          return(
+            <Redirect to={{pathname: '/login', state: {from: componentProps.location}}}/>
+          )
+        }
+    }}/>
+  )
+}
+
 function Routes() {
   return (
     <>
       <Switch>
-        <Route exact path="/home" element={<Home />} />
+        <RotaAutenticada exact path="/home" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/usuarios" element={<SignUp/>} />
-        <Route exact path="/consulta-lancamentos" element={<ConsultReleases/>} />
-        <Route exact path="/cadastro-lancamentos" element={<RegisterReleases/>} />
-        <Route exact path="/cadastro-lancamentos/:id" element={<RegisterReleases/>} />
+        <RotaAutenticada exact path="/consulta-lancamentos" element={<ConsultReleases/>} />
+        <RotaAutenticada exact path="/cadastro-lancamentos" element={<RegisterReleases/>} />
+        <RotaAutenticada exact path="/cadastro-lancamentos/:id" element={<RegisterReleases/>} />
       </Switch>
     </>
   );
